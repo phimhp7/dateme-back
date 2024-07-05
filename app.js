ebe6676c1b11814c24941ef11c8e64d8d43774c2
phimhp7 <114659925+phimhp7@users.noreply.github.com>
2024-07-05 16:08:08 +0200

    secret completed

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ http.createServer(app).listen(3001, () =>
 );
 
 mongoose
-	.connect(
-		"mongodb://cly511mgy0dhta1o229lk53zx:mCpP3VRGtwl3Pe5191jtDAF0@51.77.215.183:9000/?readPreference=primary&ssl=false",
-		{ useNewUrlParser: true, useUnifiedTopology: true }
-	)
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.log("Failed to connect to MongoDB", err));
 
